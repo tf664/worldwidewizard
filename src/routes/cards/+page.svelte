290f02414cad961721f0e9701aefc42952e9d995
t664 <tilfeldkamp@gmail.com>
2025-07-30 20:16:38 +0200

    Adds all card sources

--- src/routes/cards/+page.svelte
+++ src/routes/cards/+page.svelte
@@ -4,21 +4,31 @@
 
 	export const cardsBlue = [
 		{ id: 1, name: 'Blue One', description: 'Die Diebin', image: '/rcs/cards/blue_one.jpg' },
-		{ id: 2, name: 'Blue Two', description: '', image: '/rcs/cards/blue_two.jpg' },
-		{ id: 3, name: 'Blue Three', description: '', image: '/rcs/cards/blue_three.jpg' },
+		{ id: 2, name: 'Blue Two', description: 'Der Knecht', image: '/rcs/cards/blue_two.jpg' },
+		{ id: 3, name: 'Blue Three', description: 'Die Fischerin', image: '/rcs/cards/blue_three.jpg' },
 		{ id: 4, name: 'Blue Four', description: 'Der Bauer', image: '/rcs/cards/blue_four.jpg' },
-		{ id: 5, name: 'Blue Five', description: '', image: '/rcs/cards/blue_five.jpg' },
-		{ id: 6, name: 'Blue Six', description: '', image: '/rcs/cards/blue_six.jpg' },
+		{ id: 5, name: 'Blue Five', description: 'Die Schmiedin', image: '/rcs/cards/blue_five.jpg' },
+		{ id: 6, name: 'Blue Six', description: 'Der Händler', image: '/rcs/cards/blue_six.jpg' },
 		{
 			id: 7,
 			name: 'Blue Seven',
 			description: 'Die Priesterin',
 			image: '/rcs/cards/blue_seven.jpg'
 		},
-		{ id: 8, name: 'Blue Eight', description: '', image: '/rcs/cards/blue_eight.jpg' },
-		{ id: 9, name: 'Blue Nine', description: '', image: '/rcs/cards/blue_nine.jpg' },
-		{ id: 10, name: 'Blue Ten', description: '', image: '/rcs/cards/blue_ten.jpg' },
-		{ id: 11, name: 'Blue Eleven', description: '', image: '/rcs/cards/blue_eleven.jpg' },
+		{ id: 8, name: 'Blue Eight', description: 'Der Krieger', image: '/rcs/cards/blue_eight.jpg' },
+		{ id: 9, name: 'Blue Nine', description: 'Die Feldherrin', image: '/rcs/cards/blue_nine.jpg' },
+		{
+			id: 10,
+			name: 'Blue Ten',
+			description: 'Der Schatzmeister',
+			image: '/rcs/cards/blue_ten.jpg'
+		},
+		{
+			id: 11,
+			name: 'Blue Eleven',
+			description: 'Die Prinzessin',
+			image: '/rcs/cards/blue_eleven.jpg'
+		},
 		{ id: 12, name: 'Blue Twelve', description: 'Der König', image: '/rcs/cards/blue_twelve.jpg' },
 		{
 			id: 13,
@@ -32,23 +42,23 @@
 	}));
 
 	export const cardsRed = [
-		{ id: 1, name: 'Red One', description: '', image: '/rcs/cards/red_one.jpg' },
-		{ id: 2, name: 'Red Two', description: '', image: '/rcs/cards/red_two.jpg' },
-		{ id: 3, name: 'Red Three', description: '', image: '/rcs/cards/red_three.jpg' },
-		{ id: 4, name: 'Red Four', description: '', image: '/rcs/cards/red_four.jpg' },
+		{ id: 1, name: 'Red One', description: 'Der Dieb', image: '/rcs/cards/red_one.jpg' },
+		{ id: 2, name: 'Red Two', description: 'Die Magd', image: '/rcs/cards/red_two.jpg' },
+		{ id: 3, name: 'Red Three', description: 'Der Fischer', image: '/rcs/cards/red_three.jpg' },
+		{ id: 4, name: 'Red Four', description: 'Die Bäuerin', image: '/rcs/cards/red_four.jpg' },
 		{ id: 5, name: 'Red Five', description: 'Der Schmied', image: '/rcs/cards/red_five.jpg' },
-		{ id: 6, name: 'Red Six', description: '', image: '/rcs/cards/red_six.jpg' },
-		{ id: 7, name: 'Red Seven', description: '', image: '/rcs/cards/red_seven.jpg' },
+		{ id: 6, name: 'Red Six', description: 'Die Händlerin', image: '/rcs/cards/red_six.jpg' },
+		{ id: 7, name: 'Red Seven', description: 'Der Priester', image: '/rcs/cards/red_seven.jpg' },
 		{ id: 8, name: 'Red Eight', description: 'Die Kriegerin', image: '/rcs/cards/red_eight.jpg' },
-		{ id: 9, name: 'Red Nine', description: '', image: '/rcs/cards/red_nine.jpg' },
+		{ id: 9, name: 'Red Nine', description: 'Der Feldherr', image: '/rcs/cards/red_nine.jpg' },
 		{
 			id: 10,
 			name: 'Red Ten',
 			description: 'Die Schatzmeisterin',
 			image: '/rcs/cards/red_ten.jpg'
 		},
-		{ id: 11, name: 'Red Eleven', description: '', image: '/rcs/cards/red_eleven.jpg' },
-		{ id: 12, name: 'Red Twelve', description: '', image: '/rcs/cards/red_twelve.jpg' },
+		{ id: 11, name: 'Red Eleven', description: 'Der Prinz', image: '/rcs/cards/red_eleven.jpg' },
+		{ id: 12, name: 'Red Twelve', description: 'Die Königin', image: '/rcs/cards/red_twelve.jpg' },
 		{
 			id: 13,
 			name: 'Red Thirteen',
@@ -61,13 +71,18 @@
 	}));
 
 	export const cardsGreen = [
-		{ id: 1, name: 'Green One', description: '', image: '/rcs/cards/green_one.jpg' },
-		{ id: 2, name: 'Green Two', description: '', image: '/rcs/cards/green_two.jpg' },
+		{ id: 1, name: 'Green One', description: 'Der Dieb', image: '/rcs/cards/green_one.jpg' },
+		{ id: 2, name: 'Green Two', description: 'Die Magd', image: '/rcs/cards/green_two.jpg' },
 		{ id: 3, name: 'Green Three', description: 'Der Fischer', image: '/rcs/cards/green_three.jpg' },
 		{ id: 4, name: 'Green Four', description: 'Die Bäuerin', image: '/rcs/cards/green_four.jpg' },
-		{ id: 5, name: 'Green Five', description: '', image: '/rcs/cards/green_five.jpg' },
-		{ id: 6, name: 'Green Six', description: '', image: '/rcs/cards/green_six.jpg' },
-		{ id: 7, name: 'Green Seven', description: '', image: '/rcs/cards/green_seven.jpg' },
+		{ id: 5, name: 'Green Five', description: 'Der Schmied', image: '/rcs/cards/green_five.jpg' },
+		{ id: 6, name: 'Green Six', description: 'Die Händlerin', image: '/rcs/cards/green_six.jpg' },
+		{
+			id: 7,
+			name: 'Green Seven',
+			description: 'Der Priester',
+			image: '/rcs/cards/green_seven.jpg'
+		},
 		{
 			id: 8,
 			name: 'Green Eight',
@@ -75,15 +90,30 @@
 			image: '/rcs/cards/green_eight.jpg'
 		},
 		{ id: 9, name: 'Green Nine', description: 'Der Feldherr', image: '/rcs/cards/green_nine.jpg' },
-		{ id: 10, name: 'Green Ten', description: 'Die Standarte', image: '/rcs/cards/green_ten.jpg' },
+		{
+			id: 10,
+			name: 'Green Ten',
+			description: 'Die Schatzmeisterin',
+			image: '/rcs/cards/green_ten.jpg'
+		},
 		{
 			id: 11,
 			name: 'Green Eleven',
 			description: 'Der Prinz',
 			image: '/rcs/cards/green_eleven.jpg'
 		},
-		{ id: 12, name: 'Green Twelve', description: '', image: '/rcs/cards/green_twelve.jpg' },
-		{ id: 13, name: 'Green Thirteen', description: '', image: '/rcs/cards/green_thirteen.jpg' }
+		{
+			id: 12,
+			name: 'Green Twelve',
+			description: 'Die Königin',
+			image: '/rcs/cards/green_twelve.jpg'
+		},
+		{
+			id: 13,
+			name: 'Green Thirteen',
+			description: 'Die Standarte',
+			image: '/rcs/cards/green_thirteen.jpg'
+		}
 	].map((card) => ({
 		...card,
 		image: toWebp(`/rcs/cards/${card.name.toLowerCase().replace(/ /g, '_')}.jpg`)
@@ -92,26 +122,56 @@
 	export const cardsYellow = [
 		{ id: 1, name: 'Yellow One', description: 'Die Diebin', image: '/rcs/cards/yellow_one.jpg' },
 		{ id: 2, name: 'Yellow Two', description: 'Der Knecht', image: '/rcs/cards/yellow_two.jpg' },
-		{ id: 3, name: 'Yellow Three', description: '', image: '/rcs/cards/yellow_three.jpg' },
-		{ id: 4, name: 'Yellow Four', description: '', image: '/rcs/cards/yellow_four.jpg' },
-		{ id: 5, name: 'Yellow Five', description: '', image: '/rcs/cards/yellow_five.jpg' },
+		{
+			id: 3,
+			name: 'Yellow Three',
+			description: 'Die Fischerin',
+			image: '/rcs/cards/yellow_three.jpg'
+		},
+		{ id: 4, name: 'Yellow Four', description: 'Der Bauer', image: '/rcs/cards/yellow_four.jpg' },
+		{
+			id: 5,
+			name: 'Yellow Five',
+			description: 'Die Schmiedin',
+			image: '/rcs/cards/yellow_five.jpg'
+		},
 		{ id: 6, name: 'Yellow Six', description: 'Der Händler', image: '/rcs/cards/yellow_six.jpg' },
-		{ id: 7, name: 'Yellow Seven', description: '', image: '/rcs/cards/yellow_seven.jpg' },
+		{
+			id: 7,
+			name: 'Yellow Seven',
+			description: 'Die Priesterin',
+			image: '/rcs/cards/yellow_seven.jpg'
+		},
 		{
 			id: 8,
 			name: 'Yellow Eight',
 			description: 'Der Krieger',
 			image: '/rcs/cards/yellow_eight.jpg'
 		},
-		{ id: 9, name: 'Yellow Nine', description: '', image: '/rcs/cards/yellow_nine.jpg' },
-		{ id: 10, name: 'Yellow Ten', description: '', image: '/rcs/cards/yellow_ten.jpg' },
+		{
+			id: 9,
+			name: 'Yellow Nine',
+			description: 'Die Feldherrin',
+			image: '/rcs/cards/yellow_nine.jpg'
+		},
+		{
+			id: 10,
+			name: 'Yellow Ten',
+			description: 'Der Schatzmeister',
+			image: '/rcs/cards/yellow_ten.jpg'
+		},
 		{
 			id: 11,
 			name: 'Yellow Eleven',
 			description: 'Die Prinzessin',
 			image: '/rcs/cards/yellow_eleven.jpg'
 		},
-		{ id: 12, name: 'Yellow Twelve', description: '', image: '/rcs/cards/yellow_twelve.jpg' },
+		{
+			id: 12,
+			name: 'Yellow Twelve',
+			description: 'Der König',
+			image: '/rcs/cards/yellow_twelve.jpg'
+		},
 		{
 			id: 13,
 			name: 'Yellow Thirteen',
@@ -125,19 +185,29 @@
 
 	export const zoro = [
 		{ id: 1, name: 'Zoro One', description: 'Der Zauberer', image: '/rcs/cards/zoro_one.jpg' },
-		{ id: 2, name: 'Zoro Two', description: '', image: '/rcs/cards/zoro_two.jpg' },
-		{ id: 3, name: 'Zoro Three', description: 'Die Zauberin', image: '/rcs/cards/zoro_three.jpg' },
-		{ id: 4, name: 'Zoro Four', description: '', image: '/rcs/cards/zoro_four.jpg' }
+		{ id: 2, name: 'Zoro Two', description: 'Der Zauberer', image: '/rcs/cards/zoro_two.jpg' },
+		{ id: 3, name: 'Zoro Three', description: 'Die Hexe', image: '/rcs/cards/zoro_three.jpg' },
+		{ id: 4, name: 'Zoro Four', description: 'Die Hexe', image: '/rcs/cards/zoro_four.jpg' }
 	].map((card) => ({
 		...card,
 		image: toWebp(`/rcs/cards/${card.name.toLowerCase().replace(/ /g, '_')}.jpg`)
 	}));
 
 	export const fool = [
-		{ id: 1, name: 'Fool One Blue', description: '', image: '/rcs/cards/fool_one.jpg' },
-		{ id: 2, name: 'Fool Two Red', description: 'Der Narr', image: '/rcs/cards/fool_two.jpg' },
-		{ id: 3, name: 'Fool Three Green', description: '', image: '/rcs/cards/fool_three.jpg' },
-		{ id: 4, name: 'Fool Four Yellow', description: '', image: '/rcs/cards/fool_four.jpg' }
+		{ id: 1, name: 'Fool One Blue', description: 'Der Narr', image: '/rcs/cards/fool_one.jpg' },
+		{ id: 2, name: 'Fool Two Red', description: 'Die Närrin', image: '/rcs/cards/fool_two.jpg' },
+		{
+			id: 3,
+			name: 'Fool Three Green',
+			description: 'Der Narr',
+			image: '/rcs/cards/fool_three.jpg'
+		},
+		{
+			id: 4,
+			name: 'Fool Four Yellow',
+			description: 'Die Närrin',
+			image: '/rcs/cards/fool_four.jpg'
+		}
 	].map((card) => ({
 		...card,
 		image: toWebp(`/rcs/cards/${card.name.toLowerCase().replace(/ /g, '_')}.jpg`)
