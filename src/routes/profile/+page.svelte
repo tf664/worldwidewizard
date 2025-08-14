a8964afdaa736ea38f725119cfa875fea7477a3a
TebbeTom <tom.tebbe@iu-study.org>
2025-08-14 15:28:15 +0200

    "Websocket new try"

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script lang="ts" >
 	import { user } from '$lib/stores/user';
 
 	let name = '';
@@ -23,6 +23,28 @@
 		name = '';
 		username = '';
 	}
+
+	const express = require('express');
+	const { createServer } = require('node:http');
+	const { join } = require('node:path');
+	const { Server } = require('socket.io');
+
+	const app = express();
+	const server = createServer(app);
+	const io = new Server(server);
+	const socket = io();
+
+	app.get('/', (req, res) => {
+  		res.sendFile(join(__dirname, 'index.html'));
+	});
+
+	io.on('connection', (socket) => {
+  		console.log('a user connected');
+	});
+
+	server.listen(3000, () => {
+  		console.log('server running at http://localhost:3000');
+	});
 </script>
 
 {#if !$user}
@@ -47,5 +69,23 @@
 		<button onclick={logout} class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
 			Logout
 		</button>
+
+		<div class="mt-8">
+			<div class="bg-gray-100 h-48 rounded p-4 mb-4 overflow-y-auto text-left">
+				<!-- Chatverlauf (hier könnten Nachrichten erscheinen) -->
+				<p class="text-gray-700 italic">Noch keine Nachrichten.</p>
+			</div>
+			<div class="flex gap-2">
+				<textarea
+					class="flex-1 border rounded p-2 resize-none"
+					rows="2"
+					placeholder="Nachricht schreiben..."
+				></textarea>
+				<button class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+					Senden
+				</button>
+			</div>
+		</div>
+
 	</div>
 {/if}
